8b5e5c60c575baad1a682e2a8553d6711ce8ca52
Viktor Oliinyk <kanoute1992@gmail.com>
2023-01-05 10:50:25 +0200

    hw updates

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,7 +7,6 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const galleryPlace = document.querySelector('.gallery');
 galleryPlace.insertAdjacentHTML('beforeend', makeGalleryMarkup(galleryItems));
-galleryPlace.addEventListener('click', getOriginalImgOnClick);
 
 function makeGalleryMarkup(gallery) {
   return gallery.map(({ preview, original, description }) => {
@@ -24,25 +23,4 @@ function makeGalleryMarkup(gallery) {
     }).join('');
 };
 
-function getOriginalImgOnClick(e) {
-  e.preventDefault();
-  const currentImg = e.target.classList.contains('gallery__image');
-  const originalImg = e.target.dataset.source;
-
-  if(!currentImg) {
-    return;
-  }
-
-  openModalWindowGallery(originalImg);
-}
-
-function openModalWindowGallery(source) {
   let gallery = new SimpleLightbox('.gallery a', { scrollZoom: false});
-  gallery.on('show.simplelightbox', () => {
-      `<div class="modal">
-         <img  src="${source}" width="1280" height="800" />
-      </div>`
-  });
-};
-
-console.log(galleryItems);
